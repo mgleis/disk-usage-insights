08da8e97c3b22772a430016f603a864995ef205d
Marcel Gleis <marcel@gleis.de>
2025-04-08 14:34:19 +0200

    clean up

--- plugin/src/WpHelper.php
+++ plugin/src/WpHelper.php
@@ -1,12 +1,6 @@
 <?php
 namespace Mgleis\DiskUsageInsights;
 
-use Mgleis\DiskUsageInsights\Frontend\Controller\IndexController;
-use Mgleis\DiskUsageInsights\Frontend\Controller\ResultsController;
-use Mgleis\DiskUsageInsights\Frontend\Controller\ScanController;
-use Mgleis\DiskUsageInsights\Frontend\Controller\ScanStatusController;
-use Mgleis\DiskUsageInsights\Frontend\Controller\ScanWorkerController;
-
 class WpHelper {
 
     public static function getPluginUrl(): string {
