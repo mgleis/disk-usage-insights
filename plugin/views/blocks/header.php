ecc5637a5821232e7eb3e9fd3f878e6b7d807023
Marcel Gleis <marcel@gleis.de>
2025-11-17 08:33:08 +0100

    add animation for favourite star

--- plugin/views/blocks/header.php
+++ plugin/views/blocks/header.php
@@ -14,11 +14,12 @@
             <h1>Disk Usage Insights</h1>
             <p>Find large files in no time!</p>
 
-            <span class="dashicons-before dashicons-star-filled color-cycle"></span>
-            <a target="_blank" href="https://wordpress.org/support/plugin/disk-usage-insights/reviews/#new-post">Add a 5 star WordPress review</a>
+            <div style="float:left" class="dashicons-before dashicons-star-filled color-cycle"></div>
+                &nbsp;
+                <a target="_blank" href="https://wordpress.org/support/plugin/disk-usage-insights/reviews/#new-post">Add a 5 star WordPress review</a>
 
-            <span class="dashicons-before dashicons-editor-help"></span>
-            <a href="https://wordpress.org/support/plugin/disk-usage-insights/" target="_blank">Plugin Support Forum</a>
+                <span class="dashicons-before dashicons-editor-help"></span>
+                <a href="https://wordpress.org/support/plugin/disk-usage-insights/" target="_blank">Plugin Support Forum</a>
         </div>
     </div>
     <div style="clear:both"></div>
